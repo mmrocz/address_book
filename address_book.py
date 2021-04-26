e02eda3b6d700c29177c77a321113c0a58d11251
Michał Mroczkowski <mchlmroczkowski@gmail.com>
2021-04-26 22:30:42 +0200

    contact

--- address_book.py
+++ address_book.py
@@ -1,20 +1,26 @@
 from faker import Faker
 fake = Faker()
-name_list = []
-class address_book:
-   def __init__(self, name, address, email, company, job):
+card_list = []
+greet = 'Kontaktuj sie z'
+class Card:
+   def __init__(self, name, address, email, company, job, phone):
        self.name = name
        self.address = address
        self.email = email
        self.company = company
        self.job = job
+       self.phone = phone
+
    def __repr__(self):
-       return repr((self.name, self.address, self.email, self.company, self.job))
-bc_one=address_book(name = fake.name(), address = fake.address(), email = fake.email(), company = fake.company(), job = fake.job())
-bc_two=address_book(name = fake.name(), address = fake.address(), email = fake.email(), company = fake.company(), job = fake.job())
-bc_tree=address_book(name = fake.name(), address = fake.address(), email = fake.email(), company = fake.company(), job = fake.job())
-bc_four=address_book(name = fake.name(), address = fake.address(), email = fake.email(), company = fake.company(), job = fake.job())
-bc_five=address_book(name = fake.name(), address = fake.address(), email = fake.email(), company = fake.company(), job = fake.job())
-name_list = [bc_one, bc_two, bc_tree, bc_four, bc_five]
-sorted(name_list, key=lambda address_book: address_book.job)
-print(name_list)
+       return f'{self.name}, {self.address}, {self.email}, {self.company}, {self.job}, {self.phone}'
+
+   def contact (self):
+       return f' Kontaktuj się z {self.name}, adres {self.address}, email {self.email}'
+   @property
+   def _name_len (self):
+       return f'Długosć imienia {len(self.name)}'
+for i in range (0, 5):
+       card_list.append(Card(name = fake.name(), address = fake.address(), email = fake.email(), company = fake.company(), job=fake.job(), phone=fake.phone_number()))
+sorted(card_list, key=lambda address_book: address_book.job)
+print(card_list)
+
