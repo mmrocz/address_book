6016dd8ffd38912d331f8c6128da0dfb802a0f90
Michał Mroczkowski <mchlmroczkowski@gmail.com>
2021-04-26 23:20:21 +0200

    contact

--- address_book.py
+++ address_book.py
@@ -1,26 +1,42 @@
 from faker import Faker
 fake = Faker()
-card_list = []
-greet = 'Kontaktuj sie z'
-class Card:
-   def __init__(self, name, address, email, company, job, phone):
+
+
+class BaseContact:
+   def __init__(self, name, address, email, phone):
        self.name = name
        self.address = address
        self.email = email
-       self.company = company
-       self.job = job
        self.phone = phone
-
    def __repr__(self):
-       return f'{self.name}, {self.address}, {self.email}, {self.company}, {self.job}, {self.phone}'
-
+       return f'{self.name}, {self.address}, {self.email}, {self.phone}'
    def contact (self):
-       return f' Kontaktuj się z {self.name}, adres {self.address}, email {self.email}'
+       return f' Kontaktuj się z {self.name}, adres {self.address}, email {self.email}, telefon {self.phone}'
    @property
-   def _name_len (self):
+   def label_lenght (self):
        return f'Długosć imienia {len(self.name)}'
-for i in range (0, 5):
-       card_list.append(Card(name = fake.name(), address = fake.address(), email = fake.email(), company = fake.company(), job=fake.job(), phone=fake.phone_number()))
-sorted(card_list, key=lambda address_book: address_book.job)
-print(card_list)
+
+class BuisenesContact(BaseContact):
+    def __init__(self, job, company, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.job = job
+        self.company = company
+
+def create_contacts():
+    card_list = []
+    a = int(input('podaj typ wizytówki 2 biznesowa, 1 zwykła...'))
+    b = int(input('podaj ilość wygenerowanych wizytówek ...'))
+    if a == 1:
+        for i in range(b):
+            card_list.append(BaseContact(name=fake.name(), address=fake.address(), email=fake.email(), phone=fake.phone_number()))
+        sorted(card_list, key=lambda BaseContact: BaseContact.name)
+    else:
+        for i in range(b):
+            card_list.append(BuisenesContact(name=fake.name(), address=fake.address(), email=fake.email(), phone=fake.phone_number(), job=fake.job(),
+                                             company=fake.company()))
+        sorted(card_list, key=lambda BuisenesContact: BuisenesContact.name)
+
+    return print(card_list)
+
+create_contacts()
 
